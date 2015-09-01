82e93b1bb0a314eba8c530ad8f59ca81cfa0a239
timhuff5 <tim.huff@hp.com>
2015-09-01 16:11:25 -0500

    Updated and added the xls

--- js/plugins.js
+++ js/plugins.js
@@ -1,9 +1,3 @@
-/*================================================================================
-  Item Name: Materialize - Material Design Admin Template
-  Version: 2.1
-  Author: GeeksLabs
-  Author URL: http://www.themeforest.net/user/geekslabs
-================================================================================*/
 
 $(function() {
 
